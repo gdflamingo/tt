591d3bd47c84f33594939ceb95f8364bc8bfbb14
gdflamingo <150095120+gdflamingo@users.noreply.github.com>
2025-01-22 09:18:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
+git add requirements.txt
+git commit -m "Corrigindo requirements.txt"
+git push
 # Ler os dados
 data = pd.read_csv('vehicles.csv')
 
